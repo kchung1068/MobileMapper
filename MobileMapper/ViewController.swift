72661e78ba6f22668bab1c92b9fcca76e9dd0a64
Kyle Chung <kchung1068@jh-mba158-04.local>
2019-03-06 14:54:53 -0600

    Initial Commit

---
+++ MobileMapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileMapper
+//
+//  Created by Kyle Chung on 3/6/19.
+//  Copyright © 2019 Kyle Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
