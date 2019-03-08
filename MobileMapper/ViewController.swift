bd483e4d72b017db5bf12a2c9b7329d7708817a6
Kyle Chung <kchung1068@jh-mba158-04.d214.org>
2019-03-08 14:35:11 -0600

    set zoom

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -13,17 +13,45 @@ import MapKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var Maroon5: MKMapView!
     
+    let locationManager = CLLocationManager()
+    var CurrentLocation: CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.requestWhenInUseAuthorization(                                                                 )
+        Maroon5.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         
     }
-
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CurrentLocation = locations[0]
+        
+        print("I like wildfire")
+    }
+  
+    @IBAction func whenZoombuttonpressed(_ sender: Any) {
+        let center = CurrentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        Maroon5.setRegion(region, animated: false)
+        
+        
+    }
+    @IBAction func whenSearchbuttonpressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
 }
 
